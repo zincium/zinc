299471db9d701e0967b64d88d71912cf3dd87df9
Force Charlie <charlieio@outlook.com>
2020-10-31 20:40:03 +0800

    Add BLAKE3 base MakeSasToken and VerifySasToken

---
+++ modules/auth/signature.go
@@ -0,0 +1,47 @@
+package auth
+
+import (
+	"crypto/hmac"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"hash"
+	"time"
+
+	"github.com/zeebo/blake3"
+)
+
+// error
+var (
+	ErrSasTokenExpired  = errors.New("shared access signature token expired")
+	ErrSasTokenNotMatch = errors.New("shared access signature token not match")
+)
+
+func blake3New() hash.Hash {
+	return blake3.New()
+}
+
+// MakeSasToken create shared access signature token
+func MakeSasToken(secureKey, host, path string, expired int64) (string, error) {
+	sectext := fmt.Sprintf("%s://%s\n%d", host, path, expired)
+	h := hmac.New(blake3New, []byte(secureKey))
+	if _, err := h.Write([]byte(sectext)); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(h.Sum(nil)), nil
+}
+
+// VerifySasToken todo
+func VerifySasToken(sastoken, secureKey, host, path string, expired int64) error {
+	if time.Now().Unix() > expired {
+		return ErrSasTokenExpired
+	}
+	st, err := MakeSasToken(secureKey, host, path, expired)
+	if err != nil {
+		return err
+	}
+	if st != sastoken {
+		return ErrSasTokenNotMatch
+	}
+	return nil
+}

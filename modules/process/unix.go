ef1abf6afd0f00ae1636d31d1b18231f45828d79
Force Charlie <charlieio@outlook.com>
2021-08-07 12:46:03 +0800

    Fix process.Finalize

--- modules/process/unix.go
+++ modules/process/unix.go
@@ -8,11 +8,12 @@ import (
 )
 
 func Finalize(cmd *exec.Cmd) error {
-	if cmd == nil {
+	if cmd == nil || cmd.Process == nil || cmd.Process.Pid <= 0 {
 		return nil
 	}
-	if cmd.Process != nil && cmd.Process.Pid > 0 {
+	if cmd.SysProcAttr.Setpgid {
 		syscall.Kill(-cmd.Process.Pid, syscall.SIGTERM)
 	}
+	syscall.Kill(cmd.Process.Pid, syscall.SIGTERM)
 	return cmd.Wait()
 }

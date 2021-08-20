bdb8fcdc814711b44e4691e8272e1aeadb8cf1ae
Force Charlie <charlieio@outlook.com>
2021-08-20 23:09:16 +0800

    protobuf update

--- protocol/proto.go
+++ protocol/proto.go
@@ -1,3 +1,3 @@
-//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative --go-grpc_out=. *.proto
+//go:generate protoc -I. --go_out=. --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative --go-grpc_out=. *.proto
 
 package protocol
